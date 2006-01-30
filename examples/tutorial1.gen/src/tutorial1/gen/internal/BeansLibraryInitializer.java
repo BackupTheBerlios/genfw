ff6d0696a7d409b674ecdb33474f9421da37154c
stepper <stepper>
2006-01-30 16:07:38 +0000

    *** empty log message ***

--- examples/tutorial1.gen/src/tutorial1/gen/internal/BeansLibraryInitializer.java
+++ examples/tutorial1.gen/src/tutorial1/gen/internal/BeansLibraryInitializer.java
@@ -4,18 +4,14 @@ package tutorial1.gen.internal;
 import com.sympedia.genfw.GenLib;
 import com.sympedia.genfw.IExternalLibraryInitializer;
 
+
 /**
- * This class has to be present only to be referenced from plugin.xml!
- * @author Stepper
- *
+ * This class has to be present only to be referenced from plugin.xml
  */
 public class BeansLibraryInitializer implements IExternalLibraryInitializer
 {
-  public BeansLibraryInitializer()
-  {
-  }
-
   public void initalizeLibrary(GenLib genlib) throws Exception
   {
+    // Intentionally left empty
   }
 }
